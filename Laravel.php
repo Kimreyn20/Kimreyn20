51158fbd475f785be9df17f95151cd5e9389d741
Kimreyn20 <163563687+Kimreyn20@users.noreply.github.com>
2024-03-16 12:19:35 +0800

    Update Laravel.php

--- Laravel.php
+++ Laravel.php
@@ -1,12 +1,54 @@
-- 👋 Hi, I’m @Kimreyn20
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
+<?php
 
-<!---
-Kimreyn20/Kimreyn20 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/add/{n1}/{n2}', function ($n1,$n2) {
+    validateNumbers([$n1, $n2]);
+    $s= $n1+$n2;
+    return "The sum is ".$s;
+});
+
+Route::put('/sub/{n1}/{n2}', function ($n1,$n2) {
+    validateNumbers([$n1, $n2]);
+    $d= $n1-$n2;
+    return "The difference is ".$d;
+});
+
+Route::delete('/mul/{n1}/{n2}', function ($n1,$n2) {
+    validateNumbers([$n1, $n2]);
+    $p= $n1*$n2;
+    return "The product is ".$p;
+});
+
+Route::post('/div/{n1}/{n2}', function ($n1,$n2) {
+    validateNumbers([$n1, $n2]);
+    $q= $n1/$n2;
+    if ($n2 == 0) {
+        return "The dividor shoul not be equal to zero";}
+        else { return "The quotient is ".$q;
+}});
+
+function validateNumbers($numbers)
+{
+    $rules = ['numeric'];
+
+    foreach ($numbers as $number) {
+        validator([$number], $rules)->validate();
+    }
+}
